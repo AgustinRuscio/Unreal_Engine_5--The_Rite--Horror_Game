701faecc663d28ed3a7c536ae7931027d2a89054
Agustin Ruscio <agustin.ruscio6@gmail.com>
2024-07-18 03:31:03 -0300

    Begin level cinematic added

--- Source/TheRite/LevelsGameFlow/GameFlowGameBegin.cpp
+++ Source/TheRite/LevelsGameFlow/GameFlowGameBegin.cpp
@@ -4,6 +4,12 @@
 
 
 #include "GameFlowGameBegin.h"
+#include "MovieSceneSequencePlaybackSettings.h"
+#include "TheRite/AlexPlayerController.h"
+#include "TheRite/Characters/Alex.h"
+#include "LevelSequencePlayer.h"
+#include "LevelSequenceActor.h"
+#include "Kismet/GameplayStatics.h"
 
 AGameFlowGameBegin::AGameFlowGameBegin()
 {
@@ -13,9 +19,41 @@ AGameFlowGameBegin::AGameFlowGameBegin()
 void AGameFlowGameBegin::BeginPlay()
 {
 	Super::BeginPlay();
+
+	SetNeededValues();
+	PlayBeginSequence();
 }
 
 void AGameFlowGameBegin::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+
+void AGameFlowGameBegin::SetNeededValues()
+{
+	Player = Cast<AAlex>(UGameplayStatics::GetPlayerCharacter(GetWorld(),0));
+}
+
+void AGameFlowGameBegin::PlayBeginSequence()
+{
+	Player->ForceDisableInput();
+	
+	FMovieSceneSequencePlaybackSettings PlaybackSettings;
+	PlaybackSettings.PlayRate = 1.0f; 
+	PlaybackSettings.bAutoPlay = true;
+	PlaybackSettings.bRandomStartTime = false;
+	
+	ALevelSequenceActor* TempLevelSequenceActor = GetWorld()->SpawnActor<ALevelSequenceActor>();
+	
+	ULevelSequencePlayer* sequencePlayer =  ULevelSequencePlayer::CreateLevelSequencePlayer(GetWorld(), LS_BeginSequence,
+																				PlaybackSettings,TempLevelSequenceActor);
+
+	sequencePlayer->OnFinished.AddDynamic(this, &AGameFlowGameBegin::BeginSequenceFinished);
+	
+	sequencePlayer->Play();
+}
+
+void AGameFlowGameBegin::BeginSequenceFinished()
+{
+	Player->ForceEnableInput();
 }
--- Source/TheRite/LevelsGameFlow/GameFlowGameBegin.h
+++ Source/TheRite/LevelsGameFlow/GameFlowGameBegin.h
@@ -7,7 +7,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "TheRite/Characters/Alex.h"
 #include "GameFlowGameBegin.generated.h"
+
+class ULevelSequence;
 
 UCLASS()
 class THERITE_API AGameFlowGameBegin : public AActor
@@ -18,4 +21,18 @@ public:
 	AGameFlowGameBegin();
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
+
+private:
+	void SetNeededValues();
+	
+	void PlayBeginSequence();
+	
+	UFUNCTION()
+	void BeginSequenceFinished();
+
+private:
+	UPROPERTY(EditAnywhere, Category= "Sequence")
+	ULevelSequence* LS_BeginSequence;
+
+	AAlex* Player;
 };
