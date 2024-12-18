4ecaf61e0ee9a5c4c9cfc0028ce3e910b5a7910b
Agus Ruscio <99211308+AgustinRuscio@users.noreply.github.com>
2024-12-18 02:11:22 -0300

    New version 5.4

--- Source/TheRiteEditor.Target.cs
+++ Source/TheRiteEditor.Target.cs
@@ -8,8 +8,15 @@ public class TheRiteEditorTarget : TargetRules
 	public TheRiteEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 		WindowsPlatform.PCHMemoryAllocationFactor = 2000;
+
+		bLegacyParentIncludePaths = false;
+		CppStandard = CppStandardVersion.Cpp20;
+		WindowsPlatform.bStrictConformanceMode = true;
+		bValidateFormatStrings = true;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+
 		ExtraModuleNames.AddRange( new string[] { "TheRite" } );
 	}
 }
